bc5a70d5823dbe1bb32c31e3e0d841ac98e47d44
Abhay Kumar <abhaykumar01234@gmail.com>
2023-12-12 14:05:47 +0530

    fix: first commit

---
+++ hello/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int name;
+    cout << "Enter a name\n";
+    cin >> name;
+    cout << "number is " << name << endl;
+    return 0;
+}
